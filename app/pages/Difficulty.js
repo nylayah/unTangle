46f55432e317f9ef2cc6c7aafb3b6ffd3efd1d76
Nylayah Jones <46319759+nylayah@users.noreply.github.com>
2024-08-02 14:35:51 +0000

    deleted olf files

--- app/pages/Difficulty.js
+++
@@ -1,5 +0,0 @@
-// create a page with three functional buttons that control the diffculty of the app/game and navitgates to the game session with difficulty as a prop
-
-
-
-
--- app/pages/GameSession.js
+++
@@ -1,6 +0,0 @@
-// begin a game session based on the diffculty selected by the user
-
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Link } from 'expo-router';
-
